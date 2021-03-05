10b9f2ccbd891c5b6144516c8a2a229b24258f5d
utsmannn <kucingapes@gmail.com>
2021-03-06 03:39:16 +0700

    update 1.1.0-alpha

--- src/Application.kt
+++ src/Application.kt
@@ -45,8 +45,8 @@ fun Application.module(testing: Boolean = false) {
 
         get("/version") {
             val macOs = mapOf(
-                "version_code" to 1,
-                "version_name" to "1.0.0-alpha",
+                "version_code" to 2,
+                "version_name" to "1.1.0-alpha",
                 "changelog" to listOf(
                     "Fix enable Google Auth",
                     "Fix local data"
